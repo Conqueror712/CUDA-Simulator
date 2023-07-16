01b31ffc98f4c33886f266d43c7afa33760c25ef
Conqueror712 <Conqueror712@bupt.edu.cn>
2023-07-16 14:21:04 +0800

    cargo build demo

---
+++ cargo_demo/cuda/src/lib.rs
@@ -0,0 +1,184 @@
+#![allow(non_snake_case)]
+use cuda_sys::*;
+use std::collections::HashMap;
+use std::ffi::{CStr, CString};
+use std::os::raw::{c_char, c_int, c_uint, c_void};
+use std::sync::Mutex;
+
+lazy_static::lazy_static! {
+    static ref LIBCUDA: libloading::Library = unsafe {
+        libloading::Library::new(std::env::var("LIBCUDA").unwrap_or("/usr/lib/libcuda.so.1".to_string())).unwrap()
+    };
+    static ref TABEL: Mutex<HashMap<(CString, c_int, cuuint64_t), usize>> = Default::default();
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn cuGetProcAddress_v2(
+    symbol: *const c_char,
+    pfn: *mut *mut c_void,
+    cudaVersion: c_int,
+    flags: cuuint64_t,
+    status: *mut CUdriverProcAddressQueryResult,
+) -> CUresult {
+    let lookup: libloading::Symbol<
+        unsafe extern "C" fn(
+            *const c_char,
+            *mut *mut c_void,
+            c_int,
+            cuuint64_t,
+            *mut CUdriverProcAddressQueryResult,
+        ) -> CUresult,
+    > = LIBCUDA.get(b"cuGetProcAddress_v2").unwrap();
+
+    let res = lookup(symbol, pfn, cudaVersion, flags, status);
+
+    let symbol = CStr::from_ptr(symbol);
+
+    TABEL
+        .lock()
+        .unwrap()
+        .insert((symbol.into(), cudaVersion, flags), *pfn as _);
+
+    match (symbol.to_str().unwrap(), cudaVersion, flags) {
+        ("cuInit", 2000, 0) => {
+            *pfn = cuInit as _;
+        }
+        ("cuDeviceGetCount", 2000, 0) => {
+            *pfn = cuDeviceGetCount as _;
+        }
+        ("cuDeviceGet", 2000, 0) => {
+            *pfn = cuDeviceGet as _;
+        }
+        ("cuDeviceGetName", 2000, 0) => {
+            *pfn = cuDeviceGetName as _;
+        }
+        ("cuDeviceGetAttribute", 2000, 0) => {
+            *pfn = cuDeviceGetAttribute as _;
+        }
+        ("cuDeviceTotalMem", 3020, 0) => {
+            *pfn = cuDeviceTotalMem_v2 as _;
+        }
+        ("cuGetProcAddress", _, 0) => {
+            *pfn = cuGetProcAddress_v2 as _;
+        }
+        ("cuGetExportTable", 3000, 0) => {
+            *pfn = cuGetExportTable as _;
+        }
+        _ => {
+            eprintln!(
+                "cuGetProcAddress_v2({:?}, {:?}, {}, {}, {:?}) -> {:?}",
+                symbol,
+                pfn.as_ref(),
+                cudaVersion,
+                flags,
+                status.as_ref(),
+                res
+            );
+        }
+    }
+
+    res
+}
+
+pub unsafe extern "C" fn cuDeviceGetAttribute(
+    pi: *mut c_int,
+    attrib: CUdevice_attribute,
+    dev: CUdevice,
+) -> CUresult {
+    let func: libloading::Symbol<
+        unsafe extern "C" fn(*mut c_int, CUdevice_attribute, CUdevice) -> CUresult,
+    > = LIBCUDA.get(b"cuDeviceGetAttribute").unwrap();
+
+    let result = func(pi, attrib, dev);
+    eprintln!(
+        "cuDeviceGetAttribute(pi: {:?}, attrib: {:?}, dev: {}) -> {:?}",
+        pi.as_ref(),
+        attrib,
+        dev,
+        result
+    );
+    result
+}
+
+pub unsafe extern "C" fn cuDeviceTotalMem_v2(bytes: *mut usize, dev: CUdevice) -> CUresult {
+    let func: libloading::Symbol<unsafe extern "C" fn(*mut usize, CUdevice) -> CUresult> =
+        LIBCUDA.get(b"cuDeviceTotalMem_v2").unwrap();
+
+    let result = func(bytes, dev);
+    eprintln!(
+        "cuDeviceTotalMem(bytes: {:?}, dev {}) -> {:?}",
+        bytes.as_ref(),
+        dev,
+        result
+    );
+    result
+}
+
+pub unsafe extern "C" fn cuInit(flags: c_uint) -> CUresult {
+    let func: libloading::Symbol<unsafe extern "C" fn(c_uint) -> CUresult> =
+        LIBCUDA.get(b"cuInit").unwrap();
+
+    let result = func(flags);
+    eprintln!("cuInit(flags: {}) -> {:?}", flags, result);
+    result
+}
+
+pub unsafe extern "C" fn cuDeviceGetCount(count: *mut c_int) -> CUresult {
+    let func: libloading::Symbol<unsafe extern "C" fn(*mut c_int) -> CUresult> =
+        LIBCUDA.get(b"cuDeviceGetCount").unwrap();
+
+    let result = func(count);
+    eprintln!(
+        "cuDeviceGetCount(count: {:?}) -> {:?}",
+        count.as_ref(),
+        result
+    );
+    result
+}
+
+pub unsafe extern "C" fn cuDeviceGet(device: *mut CUdevice, ordinal: c_int) -> CUresult {
+    let func: libloading::Symbol<unsafe extern "C" fn(*mut CUdevice, c_int) -> CUresult> =
+        LIBCUDA.get(b"cuDeviceGet").unwrap();
+
+    let result = func(device, ordinal);
+    eprintln!(
+        "cuDeviceGet(device: {:?}, ordinal: {}) -> {:?}",
+        device.as_ref(),
+        ordinal,
+        result
+    );
+    result
+}
+
+pub unsafe extern "C" fn cuDeviceGetName(name: *mut c_char, len: c_int, dev: CUdevice) -> CUresult {
+    let func: libloading::Symbol<unsafe extern "C" fn(*mut c_char, c_int, CUdevice) -> CUresult> =
+        LIBCUDA.get(b"cuDeviceGetName").unwrap();
+
+    let result = func(name, len, dev);
+    eprintln!(
+        "cuDeviceGetName(name: {:?}, len: {}, dev: {}) -> {:?}",
+        CStr::from_ptr(name),
+        len,
+        dev,
+        result
+    );
+    result
+}
+
+pub unsafe extern "C" fn cuGetExportTable(
+    ppExportTable: *mut *const c_void,
+    pExportTableId: *const CUuuid,
+) -> CUresult {
+    let func: libloading::Symbol<
+        unsafe extern "C" fn(*mut *const c_void, *const CUuuid) -> CUresult,
+    > = LIBCUDA.get(b"cuGetExportTable").unwrap();
+
+    let result = func(ppExportTable, pExportTableId);
+    eprintln!(
+        "cuGetExportTable(ppExportTable: {:?}, pExportTableId: {:?}) -> {:?}",
+        ppExportTable.as_ref(),
+        pExportTableId.as_ref(),
+        result
+    );
+    result
+}
