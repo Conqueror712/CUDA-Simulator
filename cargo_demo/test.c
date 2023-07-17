f6418e3c241b021f1c9e84fa086432ff4ec9e004
Conqueror712 <Conqueror712@bupt.edu.cn>
2023-07-17 16:11:08 +0800

    feat: lib.c

--- cargo_demo/test.c
+++ cargo_demo/test.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #include <cuda.h>
 
+// 函数声明
+CUresult mock_cuInit(unsigned int flags);
+CUresult actual_cuInit(unsigned int flags);
+
+// 重定向函数
+CUresult redirect_cuInit(unsigned int flags) {
+    mock_cuInit(flags);
+    return actual_cuInit(flags);
+}
+
+// mock_cuInit 函数
+CUresult mock_cuInit(unsigned int flags) {
+    // 模拟CUDA调用
+    printf("mock_cuInit called with flags %d\n", flags);
+    return CUDA_SUCCESS;
+}
+
+// actual_cuInit 函数，用于调用 cuInit
+CUresult actual_cuInit(unsigned int flags) {
+    printf("actual_cuInit called with flags %d\n", flags);
+    return cuInit(flags);
+}
+
+// 修改 cuInitPtr 的类型
 typedef CUresult (*cuInitFunc)(unsigned int);
 
 int main() { 
@@ -8,24 +32,22 @@ int main() {
     cuInitFunc cuInitPtr;
     
     int pi; 
-    int dev;  
+    int dev;
     cuDeviceGet(&dev, 0);
     
     char* cuInitStr = (char*)"cuInit";
     
-    result = cuGetProcAddress_v2(cuInitStr, (void **)&cuInitPtr, 0, 0, NULL);
-    if (result != CUDA_SUCCESS) {
-        printf("Error: cuGetProcAddress_v2 failed with error %d\n", result);
-        return -1;  
+    // 修改 test.c 中 cuInitPtr 的获取和调用
+    result = cuGetProcAddress_v2((char*)"redirect_cuInit", (void **)&cuInitPtr, 0, 0, NULL);
+
+    if (result == CUDA_SUCCESS) {      
+        cuInitPtr(0);        
     }
       
     result = cuDeviceGetAttribute(&pi, CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK, dev); 
     
     if (result == CUDA_SUCCESS) {
         result = cuInitPtr(0);
-        // result = cuInitPtr(0x1);    // 添加 CUDA_IPC_ENABLE 标志
-        // result = cuInitPtr(0x2); // 添加 CUDA_SCHEDULE_AUTO 标志
-        // result = cuInitPtr(0x1 | 0x2);  // 同时启用IPC和自动线程调度
     }
     
     if (result != CUDA_SUCCESS) {
