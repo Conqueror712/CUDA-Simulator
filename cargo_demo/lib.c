f4371d3d3868dac2993b30731bf43ea8c0c3eee9
Conqueror712 <Conqueror712@bupt.edu.cn>
2023-07-17 15:36:00 +0800

    feat: lib.c

--- cargo_demo/lib.c
+++ cargo_demo/lib.c
@@ -1,3 +1,9 @@
+#include <cuda.h>
+
+CUresult mock_cuInit(unsigned int flags);
+CUresult actual_cuInit(unsigned int flags);
+
+// 重定向函数
 CUresult redirect_cuInit(unsigned int flags) {
     mock_cuInit(flags);
     return actual_cuInit(flags);
